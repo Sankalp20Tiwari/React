b17b1a592799828ca3ddf073396fcfeaf4e72650
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2024-09-03 13:12:08 +0530

    hover

--- 07reactrouter/src/components/Footer/Footer.jsx
+++ 07reactrouter/src/components/Footer/Footer.jsx
@@ -20,24 +20,24 @@ export default function Footer() {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
-                                    <Link to="/" className="hover:underline">
+                                    <Link to="/" className="hover:underline hover:text-orange-600">
                                         Home
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link to="/about" className="hover:underline">
+                                    <Link to="/about" className="hover:underline hover:text-orange-600">
                                         About
                                     </Link>
                                 </li>
                             </ul>
                         </div>
                         <div>
-                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase hover:text-orange-600">Follow us</h2>
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <a
                                         href="https://github.com/hiteshchoudhary"
-                                        className="hover:underline"
+                                        className="hover:underline hover:text-orange-600"
                                         target="_blank"
                                         rel="noreferrer"
                                     >
@@ -45,7 +45,7 @@ export default function Footer() {
                                     </a>
                                 </li>
                                 <li>
-                                    <Link to="/" className="hover:underline">
+                                    <Link to="/" className="hover:underline hover:text-orange-600">
                                         Discord
                                     </Link>
                                 </li>
@@ -55,12 +55,12 @@ export default function Footer() {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
-                                    <Link to="#" className="hover:underline">
+                                    <Link to="#" className="hover:underline hover:text-orange-600">
                                         Privacy Policy
                                     </Link>
                                 </li>
                                 <li>
-                                    <Link to="#" className="hover:underline">
+                                    <Link to="#" className="hover:underline hover:text-orange-600">
                                         Terms &amp; Conditions
                                     </Link>
                                 </li>
@@ -72,7 +72,7 @@ export default function Footer() {
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
                         © 2024
-                        <a href="https://hiteshchoudhary.com/" className="hover:underline">
+                        <a href="https://hiteshchoudhary.com/" className="hover:underline  hover:text-orange-600">
                             SankalpTiwari
                         </a>
                         . All Rights Reserved.
